a04bded8fcaef411190ee0ba6303288de698f2e0
venkatgattu <47410674+venkatgattu@users.noreply.github.com>
2019-03-05 21:09:13 +0530

    Update Hello2.java

--- src/Hello2.java
+++ src/Hello2.java
@@ -3,5 +3,6 @@ class Hello2
    public static void main(String args[])
    {
       System.out.println("hello from class2..modified..2nd time");
+       System.out.println("hello from class2..modified..2nd time");
    }
 }
